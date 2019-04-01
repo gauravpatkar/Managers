702efd9a1eb030c5d5d0a05708bd6c4f0536ea6f
unknown <L00144821@student.lyit.ie>
2019-04-01 12:15:32 +0100

    commit merge data to repo

--- Managers.R
+++ Managers.R
@@ -1,8 +1,4 @@
-# This script builds the managers dataset
-# and populates it with data
 
-# Load data from previous session
-column_names <- c("Date", "Country", "Gender", "Age", "Q1", "Q2", "Q3", "Q4", "Q5")
 
 # Enter data into vectors before constructing the data frame
 date_col <- c("2018-15-10", "2018-01-11", "2018-21-10", "2018-28-10", "2018-01-05")
@@ -15,11 +11,17 @@ q3_col <- c(5, 2, 5, 4, 1)
 q4_col <- c(5, 5, 5, NA, 2) # NA is inserted in place of the missing data for this attribute
 q5_col <- c(5, 5, 2, NA, 1)
 
+# Create column names for data frame
+column_names <- c("Date", "Country", "Gender", "Age", "Q1", "Q2", "Q3", "Q4", "Q5")
+
 # Construct a data frame using the data from all vectors above
 managers <- data.frame(date_col, country_col, gender_col, age_col, q1_col, q2_col, q3_col, q4_col, q5_col)
 
 # Add column names to data frame using column_names vector
 colnames(managers) <- column_names
+
+# Refer to blackboard notes for an understanding
+# why the following steps are required
 
 # Recode the incorrect 'age' data to NA
 managers$Age[managers$Age == 99] <- NA
@@ -29,17 +31,17 @@ managers$Age[managers$Age == 99] <- NA
 # <= 25 = Young
 # >= 26 & <= 44 = Middle Aged
 # >= 45 = Elderly
-# We will also recode age 'NA' to Elder
+# We will also recode age 'NA' to Elderly
 
-managers$AgeCat[managers$Age >= 45] <- "Elder"
+managers$AgeCat[managers$Age >= 45] <- "Elderly"
 managers$AgeCat[managers$Age >= 26 & managers$Age <= 44] <- "Middle Aged"
 managers$AgeCat[managers$Age <= 25] <- "Young"
-managers$AgeCat[is.na(managers$Age)] <- "Elder"
+managers$AgeCat[is.na(managers$Age)] <- "Elderly"
 
 # Recode AgeCat so that is ordinal and factored with the
 # order Young, Middle aged, Elder
 # We'll srore the ordinal factored data in variable 'AgeCat'
-AgeCat <- factor(managers$AgeCat, order = TRUE, levels = c("Young", "Middle Aged", "Elder"))
+AgeCat <- factor(managers$AgeCat, order = TRUE, levels = c("Young", "Middle Aged", "Elderly"))
 
 # Replace managers's AgeCat attribute with newly ordinal foctored data
 managers$AgeCat <- AgeCat
@@ -70,3 +72,64 @@ names(managers)[11] <- "Answer total"
 
 # Show 
 str(managers)
+
+new_managers_data <- read.csv("MoreData.csv")
+str(new_managers_data)
+
+names(new_managers_data)
+include_list <- new_managers_data[c("Date","Country"...)]
+
+include_list <- new_managers_data[c(6,3,7,4,8:12)]
+
+include_list <- subset(new_managers_data, select = c(6,3,7,4,8:12))
+include_list
+
+rbind(managers, include_list)
+
+blank_vectors <- c("AgeCat", "Answer total", "mean value")
+include_list [,blank_vectors] <- NA
+
+attach(include_list)
+include_list$AgeCat[Age >=45] <-"Elder"
+include_list$AgeCat[Age >=26 & Age <=44] <-"Middle Age"
+include_list$AgeCat[Age >=25] <-"young"
+include_list$AgeCat[is.na(Age)] <-"Elder"
+detach(include_list)
+
+#calculate 
+attach(include_list)
+include_list$'Answer total' <- Q1 + Q2 + Q3 + Q4 + Q5
+
+#calculate mean value for each row
+include_list$'mean value' <- rowMeans(include_list[5:9])
+
+detach(include_list)
+
+#default date format in R is yyyy-mm-dd
+#in the imported data frame using %d %m %y etc
+#R then puts the date format into default yyyy-mm-dd
+#format when using Data data type
+
+
+#convert date structure for include_list data frame
+attach(managers)
+converted_date <- as.Date(Date, "%Y-%d-%m")
+converted_date <- format(converted_date,"%d/%m/%Y")
+converted_date
+detach(managers)
+managers$Date <- converted_date
+managers
+attach(include_list)
+converted_date <- as.Date(Date, "%m/%d/%Y")
+converted_date <- format(converted_date, "%d/%m/%Y")
+converted_date
+detach(include_list)
+include_list$Date <- converted_date
+include_list
+
+
+
+managers <- rbind(managers, include_list)
+managers
+
+
